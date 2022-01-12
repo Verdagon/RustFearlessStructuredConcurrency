27339a7550b2108b3143839af45adc1e1cba1326
Evan Ovadia <verdagon@gmail.com>
2022-01-12 01:04:42 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use async_scoped::*;
+
+#[async_std::main]
+async fn main() {
+    scoped_futures().await;
+}
+
+async fn scoped_futures() {
+    let not_copy = String::from("hello");
+    let not_copy_ref = &not_copy;
+
+    let ((), vals) = Scope::scope_and_block(|s: &mut async_scoped::Scope<'_, (), async_scoped::spawner::use_async_std::AsyncStd>| {
+        for _ in 0..10 {
+            let proc = || async {
+                println!("Running a task! {:?}", not_copy_ref);
+            };
+            s.spawn(proc());
+        }
+    });
+
+    assert_eq!(vals.len(), 10);
+}
