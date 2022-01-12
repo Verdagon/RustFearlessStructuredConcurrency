1734c0a7a2fe779390941a3bfe90e33efc698dfa
Evan Ovadia <verdagon@gmail.com>
2022-01-12 01:07:19 -0500

    Shows workaround for compiler error

--- src/main.rs
+++ src/main.rs
@@ -5,34 +5,14 @@ async fn main() {
     scoped_futures().await;
 }
 
-trait Spaceship { }
-
-struct Firefly { }
-impl Spaceship for Firefly { }
-
-struct World {
-    some_int: i64,
-    ship: Box<dyn Spaceship>
-    // ... plus many other fields, which contain any number
-    // of trait objects that may or may not have + Sync
-}
-impl World {
-    fn new() -> World {
-        World {
-            some_int: 0,
-            ship: Box::new(Firefly { })
-        }
-    }
-}
-
 async fn scoped_futures() {
-    let not_copy = World::new();
+    let not_copy = String::from("hello");
     let not_copy_ref = &not_copy;
 
     let ((), vals) = Scope::scope_and_block(|s: &mut async_scoped::Scope<'_, (), async_scoped::spawner::use_async_std::AsyncStd>| {
         for _ in 0..10 {
             let proc = || async {
-                println!("Running a task! {:?}", not_copy_ref.some_int);
+                println!("Running a task! {:?}", not_copy_ref);
             };
             s.spawn(proc());
         }
